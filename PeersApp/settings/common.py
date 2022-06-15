c5d062176032d167ce4bd62bdd757c70f1aaf381
RojiTech Global <rojitech9@gmail.com>
2022-06-15 15:17:49 +0100

    added some

--- PeersApp/settings/common.py
+++ PeersApp/settings/common.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-g&@3na06dum*%=qc6yf5q3yewk!xllvz9wt5=prdhe-#8vbom%"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
