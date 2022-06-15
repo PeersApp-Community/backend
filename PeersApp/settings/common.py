bf9c6a044c7bcb23d373e7f165febf9528cc7fe3
RojiTech Global <rojitech9@gmail.com>
2022-06-15 22:24:09 +0100

    added

--- PeersApp/settings/common.py
+++ PeersApp/settings/common.py
@@ -142,6 +142,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "base.User"
 
 REST_FRAMEWORK = {
+    "COERCE_DECIMAL_TO_STRING": False,
     "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
