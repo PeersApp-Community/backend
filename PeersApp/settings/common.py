a27901560e4489a142a6325af77e5aa06256ce5b
RojiTech Global <rojitech9@gmail.com>
2022-06-15 23:11:39 +0100

    added new

--- PeersApp/settings/common.py
+++ PeersApp/settings/common.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     # "debug_toolbar.middleware.DebugToolbarMiddleware",
     # ...
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
