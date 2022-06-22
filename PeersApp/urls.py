c6bc4024840b68d3a0f1da51ee2e74f2ac7c473e
roji-tech <rojitech9@gmail.com>
2022-06-22 06:41:32 +0100

    edit

--- PeersApp/urls.py
+++ PeersApp/urls.py
@@ -24,9 +24,7 @@ urlpatterns = [
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
-    # path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    # path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    # path("__debug__/", include("debug_toolbar.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
     path("docs/", include_docs_urls(title="PeersApp API")),
     path(
         "api/schema/",
@@ -35,8 +33,11 @@ urlpatterns = [
         ),
         name="PeersApp-schema",
     ),
+    # path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    # path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
     # urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
