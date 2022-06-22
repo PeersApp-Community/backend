28e0c465c6776034885187001de45431cf911181
roji-tech <rojitech9@gmail.com>
2022-06-22 06:29:22 +0100

    edit

--- PeersApp/urls.py
+++ PeersApp/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path("api/", include("peers_api.urls")),
     path("base/", include("base.urls")),
     #
-    path("auth/", include("djoser.urls"), namespace="auth"),
+    path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("__debug__/", include("debug_toolbar.urls")),
