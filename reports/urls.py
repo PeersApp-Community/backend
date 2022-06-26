044e286a6802b2c219e61363e28d9dbbc0f96509
roji-tech <rojitech9@gmail.com>
2022-06-26 21:54:31 +0100

    added reports

--- reports/urls.py
+++ reports/urls.py
@@ -2,6 +2,6 @@ from .views import DataListAPIView, RatingListAPIView
 from django.urls import path
 
 urlpatterns = [
-    path("ratings", RatingListAPIView.as_view()),
-    path("info", DataListAPIView.as_view()),
+    path("ratings/", RatingListAPIView.as_view()),
+    path("info/", DataListAPIView.as_view()),
 ]
