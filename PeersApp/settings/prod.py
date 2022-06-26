6b206d3246c2445f372654fc09d8710b52972b98
roji-tech <rojitech9@gmail.com>
2022-06-26 11:02:20 +0100

    fixed profile

--- PeersApp/settings/prod.py
+++ PeersApp/settings/prod.py
@@ -6,7 +6,7 @@ import dj_database_url
 
 SECRET_KEY = os.environ["SECRET_KEY"]
 
-# DEBUG = os.environ["DEBUG"]
+DEBUG = os.environ["DEBUG"]
 
 ALLOWED_HOSTS = ["https://peers-app-sh.herokuapp.com"]
 
