c2c83c5d34c677985d5dc74a4ae0b1a1a142a905
roji-tech <rojitech9@gmail.com>
2022-06-29 00:36:50 +0100

    message

--- base/signals/handlers.py
+++ base/signals/handlers.py
@@ -8,5 +8,5 @@ from base.models import OTP, Profile
 def create_customer_for_new_user(sender, **kwargs):
     if kwargs["created"]:
         Profile.objects.create(user=kwargs["instance"])
-        OTP.objects.create(user=kwargs["instance"])
+        OTP.objects.create(user_id=kwargs["instance"].id)
         print("user created")
