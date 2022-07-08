7275d1e91f47bfdc6fe7f480065da10d8090aa60
roji-tech <rojitech9@gmail.com>
2022-07-08 21:56:09 +0100

    added

--- PeersApp/urls.py
+++ PeersApp/urls.py
@@ -34,10 +34,12 @@ urlpatterns = [
         "favicon.ico",
         RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
     ),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
-# if settings.DEBUG:
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # if settings.DEBUG:
 #     urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
